db984f835d92f295fc2fbcccaaa3e0396b638c0b
Ji Chi <chiji.zju@gmail.com>
2016-07-29 17:39:39 +0800

    Update localhost2ip.js

--- localhost2ip.js
+++ localhost2ip.js
@@ -1,11 +1,15 @@
 function getmyIP(){
     // to be done!!!
     return "192.168.1.57";
+    // return "192.168.3.4";
+    // return "localhost";
 }
 
-localip = getmyIP();
-// 替换id为baseurl的href.
-window.onload= function(){
-    var myreplace=document.getElementById('baseurl').href;
-    document.getElementById('baseurl').href = myreplace.replace("localhost",localip);
-} 
+function changeip(){
+    localip = getmyIP();
+    // 替换id为baseurl的href.
+    window.onload= function(){
+        var myreplace=document.getElementById('baseurl').href;
+        document.getElementById('baseurl').href = myreplace.replace("localhost",localip);
+    } 
+}
